61161053fe06b0e94dd6b1cffd91027b6bbba094
Student User <student@workstation.lab.example.com>
2025-11-13 14:16:31 -0500

    vaya toalla

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-I<HTML>
+<HTML>
 <HEAD><TITLE> hola PHP </TITLE></HEAD>
-<BODY>
-<?php echo '<p> Hola caracola></p>' ; ?>
+<BODY style="background-color: #f0f0f0;">
+<?php echo '<p> <marquee>Hello caracola></marquee></p>' ; ?>
 
 </BODY>
 </HTML>
